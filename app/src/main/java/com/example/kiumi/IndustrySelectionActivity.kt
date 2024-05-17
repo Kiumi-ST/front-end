cd128ab0950175af2c540774c72f6a800ac940d2
dalpaeng6 <20220876@duksung.ac.kr>
2024-05-18 03:26:35 +0900

    초기설정

---
+++ app/src/main/java/com/example/kiumi/IndustrySelectionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kiumi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IndustrySelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_industry_selection)
+    }
+}
--- app/src/main/java/com/example/kiumi/MainActivity.kt
+++ app/src/main/java/com/example/kiumi/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.kiumi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
+import com.example.kiumi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        supportActionBar?.setCustomView(R.layout.toolbar)
+        //supportActionBar?.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼 활성화
+
+        val toolbarView = supportActionBar?.customView
+        val homeIcon = toolbarView?.findViewById<ImageView>(R.id.home_icon)
+        val titleView = toolbarView?.findViewById<TextView>(R.id.toolbar_title)
+
+        titleView?.text = "키오스크 배우미"
+
+        homeIcon?.setOnClickListener {
+            // 홈 아이콘 클릭 시 동작 구현
+        }
+
+
+
+
+        findViewById<Button>(R.id.btnPractice).setOnClickListener {
+            startActivity(Intent(this, IndustrySelectionActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnHelp).setOnClickListener {
+            startActivity(Intent(this, HelpActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnSurvey).setOnClickListener {
+            startActivity(Intent(this, SurveyActivity::class.java))
+        }
     }
 }
