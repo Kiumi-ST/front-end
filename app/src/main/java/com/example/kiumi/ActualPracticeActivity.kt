be1599867299b6e5cddadf1b92caaa5b904a4901
박유정 <p_1321@duksung.ac.kr>
2024-06-27 03:15:36 +0900

    feat: 연습 방식 선택 버튼 페이지 연결

--- app/src/main/java/com/example/kiumi/ActualPracticeActivity.kt
+++ app/src/main/java/com/example/kiumi/ActualPracticeActivity.kt
@@ -1,4 +1,11 @@
 package com.example.kiumi
 
-class ActualPracticeActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActualPracticeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actual_practice)
+    }
 }
--- app/src/main/java/com/example/kiumi/TutorialActivity.kt
+++ app/src/main/java/com/example/kiumi/TutorialActivity.kt
@@ -1,4 +1,11 @@
 package com.example.kiumi
 
-class TutorialActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TutorialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial)
+    }
 }
