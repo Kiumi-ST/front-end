fbf25abc82936b0bf7f30487e0f0c871070a1661
sunflwwer <ghosti007@naver.com>
2024-07-25 04:57:40 +0900

    [추가] 개선안 대기, 메인, 장소 선택 페이지 추가 (2024.07.25)

---
+++ app/src/main/java/com/example/kiumi/ProposalPlaceSelection.kt
@@ -0,0 +1,15 @@
+package com.example.kiumi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProposalPlaceSelection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_proposal_place_selection)
+    }
+}
