92ecef923553f081a406e738d56bd0122be10989
박유정 <p_1321@duksung.ac.kr>
2024-07-25 18:37:51 +0900

    feat: 실전연습 버거 및 단품 팝업 ui 구현

--- app/src/main/java/com/example/kiumi/ActualPracticePlaceSelectionActivity.kt
+++ app/src/main/java/com/example/kiumi/ActualPracticePlaceSelectionActivity.kt
@@ -2,14 +2,22 @@ package com.example.kiumi
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ActualPracticePlaceSelectionActivity : AppCompatActivity() {
+    private var quantity = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actual_practice_place_selection)
+
+        val popupBurgerSelectionContainer: RelativeLayout = findViewById(R.id.popup_burger_selection)
+        val popupSingBurgerContainer: RelativeLayout = findViewById(R.id.popup_single_burger)
+        val quantityText: TextView = findViewById(R.id.quantity)
 
         findViewById<LinearLayout>(R.id.button_points).setOnClickListener {
             val intent = Intent(
@@ -33,6 +41,53 @@ class ActualPracticePlaceSelectionActivity : AppCompatActivity() {
 
         // 도움 기능 버튼 클릭 시
         findViewById<LinearLayout>(R.id.linearLayoutHelp).setOnClickListener {
+        }
+
+        // 영어 버튼 클릭 시 (임시로 세트/단품 팝업 띄우기)
+        findViewById<Button>(R.id.button_english).setOnClickListener {
+            popupBurgerSelectionContainer.visibility = View.VISIBLE
+        }
+
+        // 세트-단품 팝업
+        findViewById<LinearLayout>(R.id.button_set).setOnClickListener {
+            val intent = Intent(
+                this@ActualPracticePlaceSelectionActivity,
+                ActualPracticeQRSuccess::class.java
+            )
+            startActivity(intent)
+            popupBurgerSelectionContainer.visibility = View.GONE
+        }
+
+        findViewById<LinearLayout>(R.id.button_single).setOnClickListener {
+            popupSingBurgerContainer.visibility = View.VISIBLE
+            popupBurgerSelectionContainer.visibility = View.GONE
+        }
+
+        findViewById<Button>(R.id.button_cancel).setOnClickListener {
+            popupBurgerSelectionContainer.visibility = View.GONE
+        }
+
+        // 단품 팝업
+        // 수량 변경 버튼
+        findViewById<Button>(R.id.button_decrease_quantity).setOnClickListener {
+            if (quantity > 1) {
+                quantity--
+                quantityText.text = quantity.toString()
+            }
+        }
+        findViewById<Button>(R.id.button_increase_quantity).setOnClickListener {
+            quantity++
+            quantityText.text = quantity.toString()
+        }
+
+        // 단품 장바구니 추가
+        findViewById<Button>(R.id.button_add_to_cart).setOnClickListener {
+            val intent = Intent(
+                this@ActualPracticePlaceSelectionActivity,
+                ActualPracticeActivity::class.java
+            )
+            startActivity(intent)
+            popupSingBurgerContainer.visibility = View.GONE
         }
 
     }
