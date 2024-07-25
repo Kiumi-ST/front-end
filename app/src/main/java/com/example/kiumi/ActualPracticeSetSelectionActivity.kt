ff3c9b11b00d967e9ac8bccf8d1291d87997eef6
박유정 <p_1321@duksung.ac.kr>
2024-07-25 22:38:17 +0900

    feat: 실전연습 버거 세트 첫 화면 ui 구현

---
+++ app/src/main/java/com/example/kiumi/ActualPracticeSetSelectionActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kiumi
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+
+class ActualPracticeSetSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actual_practice_set_selection)
+    }
+}
