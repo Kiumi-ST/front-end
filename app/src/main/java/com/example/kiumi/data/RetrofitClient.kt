8dfd51d621523ffb3b7397e886465c15ac042453
박유정 <p_1321@duksung.ac.kr>
2024-09-28 20:10:34 +0900

    fix: 서버 주소 변경

--- app/src/main/java/com/example/kiumi/data/RetrofitClient.kt
+++ app/src/main/java/com/example/kiumi/data/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
 
     // Spring boot 서버 연동
-    private const val SPRING_BOOT_BASE_URL = "http://192.168.0.8:8080/" // http://(본인 노트북 IPv4 주소):8080/
+    private const val SPRING_BOOT_BASE_URL = "http://43.202.235.227:8080/"
 
     val springBootRetrofit: Retrofit = Retrofit.Builder()
         .baseUrl(SPRING_BOOT_BASE_URL)
@@ -18,7 +18,7 @@ object RetrofitClient {
     val springBootApiService: SpringBootApiService = springBootRetrofit.create(SpringBootApiService::class.java)
 
     // Flask 서버 연동
-    private const val FLASK_BASE_URL = "http://192.168.0.8:8000/"
+    private const val FLASK_BASE_URL = "https://kiumi-ef992.du.r.appspot.com"
 
     val flaskRetrofit: Retrofit = Retrofit.Builder()
         .baseUrl(FLASK_BASE_URL)
